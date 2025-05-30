0003b3da529c06c0634c291adfd6689a4e280b45
halyavkaia <lordmira002@gmail.com>
2025-05-31 00:10:46 +0300

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+n = int(input())
+target = ['white' if i % 2 == 0 else 'black' for i in range(n)]
+dq = deque()
+for i in range(n-1, -1, -1):
+    if dq:
+        dq.appendleft(dq.pop())
+    dq.appendleft(target[i])
+print(' '.join(dq))
