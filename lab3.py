07cbd97fe5ae37a1eae610b2669f45bdaae87c3d
halyavkaia <lordmira002@gmail.com>
2025-06-09 21:27:13 +0300

    lab3.py

--- lab3.py
+++ lab3.py
@@ -1,8 +1,8 @@
 from collections import deque
-#Халявка Иван Андреевич#
 n = int(input())
 target = ['white' if i % 2 == 0 else 'black' for i in range(n)]
 dq = deque()
+print ('Халявка Иван Андреевич 090301ПОВа-о24')
 for i in range(n-1, -1, -1):
     if dq:
         dq.appendleft(dq.pop())
